926f0fcdae2133dddc7cb72f7675b6fe75d995ef
alvinkenyagah <alvin47k@gmail.com>
2024-09-01 22:22:56 +0300

    toggle form visibility

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
-import PolicyForm from "../components/policyForm";
+import PolicyForm from "../components/PolicyForm";
 import PolicyTable from "../components/PolicyTable";
 
-
 function HomePage() {
-    const [policies, setPolicies] = useState([]);
-  
-    const addPolicy = (newPolicy) => {
-      setPolicies([...policies, newPolicy]);
-    };
-  
-    return (
-      <div className="App">
-        <h1>Policy Management</h1>
-        <PolicyForm onPolicyAdded={addPolicy} />
-        <br/>
-        <PolicyTable policies={policies} setPolicies={setPolicies} />
-      </div>
-    );
-  }
-  
-export default HomePage
+  const [policies, setPolicies] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  const addPolicy = (newPolicy) => {
+    setPolicies([...policies, newPolicy]);
+  };
+
+  const toggleFormVisibility = () => {
+    setShowForm(!showForm);
+  };
+
+  return (
+    <div className="App">
+      <h1>Policy Management</h1>
+      <button onClick={toggleFormVisibility}>
+        {showForm ? 'Hide Form' : 'Show Form'}
+      </button>
+      <br />
+      {showForm && <PolicyForm onPolicyAdded={addPolicy} />}
+      <br />
+      <PolicyTable policies={policies} setPolicies={setPolicies} />
+    </div>
+  );
+}
+
+export default HomePage;
